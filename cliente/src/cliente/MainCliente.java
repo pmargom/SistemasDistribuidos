91d5262048e791a98de28e3654edebf71f5c0e44
Pedro Martín <pmargom@gmail.com>
2015-10-26 20:49:56 +0000

    Añadido ficheros de Fermín

--- cliente/src/cliente/MainCliente.java
+++ cliente/src/cliente/MainCliente.java
@@ -1,24 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package cliente;
 
-/**
- *
- * @author pedro
- */
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+import common.Gui;
+import common.IServidor;
+import common.Mensaje;
+
+
 public class MainCliente {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-    
-    
+	private static int miSesion = 0;
+	private static IServidor servidor;
+	
+	public static void main(String[] args) throws Exception {
+            Registry registry = LocateRegistry.getRegistry();
+            servidor = (IServidor)registry.lookup("Pepito");
+
+            gui();
+	}
+	
+	private static void gui() throws RemoteException {
+            int opt = 0;
+
+            do {
+                opt = Gui.menu("Menu Principal", 
+                new String[]{ 
+                    "Autenticarse", 
+                    "Agregar Contacto", 
+                    "Enviar Mensaje",
+                    "Recibir Mensajes",
+                    "Salir" });
+
+                switch (opt) {
+                    case 0: autenticarse(); break;
+                    case 1: agregarContacto(); break;
+                    case 2: enviarMensaje(); break;
+                    case 3: recibirMensajes(); break;
+                }
+            }
+            while (opt != 4);
+	}
+	
+	private static void autenticarse() throws RemoteException {
+            String nombre = Gui.input("Autenticarse", "Ingrese su nombre: ");
+
+            if (nombre != null && !nombre.isEmpty()) {
+                //Primera llamada remota:
+                miSesion = servidor.autenticar(nombre);
+            }
+	}
+	
+	private static void agregarContacto() throws RemoteException {
+            String contacto = Gui.input("Agregar Contacto", "Ingrese el contacto: ");
+
+            if (contacto != null && !contacto.isEmpty()) {
+                int suSesion = servidor.agregar(contacto, miSesion);
+                System.out.println("La sesion de " + contacto + " es " + suSesion);
+            }
+	}
+	
+	private static void recibirMensajes() throws RemoteException {
+            System.out.println("=== Mensajes recibidos ===");
+
+            List<Mensaje> mensajes = servidor.recibir(miSesion);
+
+            for (Mensaje mensaje : mensajes) {
+                System.out.println("De " + mensaje.getRemitente());
+                System.out.println("\t" + mensaje.getCuerpo() + "\n");
+            }
+
+            System.out.println();
+
+            servidor.limpiarBuffer(miSesion);
+	}
+	
+	private static void enviarMensaje() throws RemoteException {
+            String opts[] = Gui.input("Enviar Mensaje", 
+            new String[]{ "Ingrese la sesion del contacto: ", "Ingrese el mensaje: "});
+
+            int suSesion = Integer.parseInt(opts[0]);
+            String mensaje = opts[1];
+
+            servidor.enviar(mensaje, miSesion, suSesion);
+	}
 }
--- servidor/src/servidor/MainServidor.java
+++ servidor/src/servidor/MainServidor.java
@@ -8,9 +8,6 @@ package servidor;
 
 import common.IServidor;
 import common.Utils;
-import java.rmi.AlreadyBoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
